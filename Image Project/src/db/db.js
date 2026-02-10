0eea685823ebb5120aacaa7336e3effc9ed5c2b5
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-02-11 05:08:37 +0530

    security reason

--- Image Project/src/db/db.js
+++ Image Project/src/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 async function connectDb(params) {
 
-  await mongoose.connect("mongodb+srv://user-tushar:7sUwMDX2Bag1A3IS@cluster0.sgjjbia.mongodb.net/project-1")
+  await mongoose.connect("URL")
 
   console.log("connected to DB")
   
--- simple/src/db/db.js
+++ simple/src/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
 
-  await mongoose.connect("mongodb+srv://user-tushar:7sUwMDX2Bag1A3IS@cluster0.sgjjbia.mongodb.net/halley")
+  await mongoose.connect("URL")
 
   console.log('Connected to DB')
   
